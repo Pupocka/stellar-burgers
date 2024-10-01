e0554bf876e8ca9e28d318b86ddcbbaac618c30b
Pupocka <d.melnitsa@yandex.ru>
2024-10-01 11:46:13 +0300

    add ingredientsSlice

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -5,26 +5,34 @@ import { Preloader } from '@ui';
 
 type TProtectedRouteProps = {
   onlyUnAuth?: boolean;
+  children?: React.ReactNode;
 };
 
-export const ProtectedRoute = ({ onlyUnAuth }: TProtectedRouteProps) =>
+export const ProtectedRoute = ({
+  onlyUnAuth,
+  children
+}: TProtectedRouteProps) =>
   // const isAuthChecked = useSelector(isAuthCheckedSelector);
   // const user = useSelector(userDataSelector);
   // const location = useLocation();
 
   // if (!isAuthChecked) {
+  // console.log('WAIT USER CHECKOUT')
   //   return <Preloader />;
   // }
 
   // if (!onlyUnAuth && !user) {
-  //   return <Navigate replace to='/login' state={{ from: location }}/>;
+  // console.log('NAVIGATE FROM PAGE TO LOGIN')
+  //   return <Navigate replace to={'/login'} state={{ from: {...location, background: location.state?.background, state: null}}} />;
   // }
 
   // if (onlyUnAuth && user) {
+  // console.log('NAVIGATE FROM LOGIN TO INDEX')
   //   const from = location.state?.from || { pathname: '/' };
-  //   return <Navigate replace to={from} />;
+  //   const backgroundLocation = location.state?.from?background || null;
+  //   return <Navigate replace to={from} state = {{background: backgroundLocation}} />;
   // }
 
-  null;
+  children;
 
 export default ProtectedRoute;
--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -13,44 +13,38 @@ import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import {
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+  useParams
+} from 'react-router-dom';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
+import { useEffect } from 'react';
+import { getIngredients } from '../../services/slices/ingredientsSlice';
+import { useDispatch } from '../../services/store';
 
 const App = () => {
   const location = useLocation();
+  const { number } = useParams();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const backgroundLocation = location.state?.background;
+  useEffect(() => {
+    dispatch(getIngredients());
+  }, []);
+
+  const background = location.state?.background;
 
   return (
     <>
       <div className={styles.app}>
         <AppHeader />
-        <Routes location={backgroundLocation || location}>
+        <Routes location={background || location}>
           <Route path='/' element={<ConstructorPage />} />
-          <Route path='/feed' element={<Feed />}>
-            <Route
-              path=':number'
-              element={
-                <Modal
-                  title={`Информация о заказе`}
-                  onClose={() => window.history.back()}
-                >
-                  <OrderInfo />
-                </Modal>
-              }
-            />
-          </Route>
-          <Route
-            path='/ingredients/:id'
-            element={
-              <Modal
-                title={`#${location.pathname.split('/')[2]}`}
-                onClose={() => window.history.back()}
-              >
-                <IngredientDetails />
-              </Modal>
-            }
-          />
+          <Route path='/feed' element={<Feed />} />
+
           <Route element={<ProtectedRoute onlyUnAuth />}>
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
@@ -58,23 +52,50 @@ const App = () => {
           </Route>
           <Route element={<ProtectedRoute />}>
             <Route path='/reset-password' element={<ResetPassword />} />
-            <Route path='/profile' element={<Profile />}>
-              <Route
-                path='orders/:number'
-                element={
-                  <Modal
-                    title={`#${location.pathname.split('/')[3]}`}
-                    onClose={() => window.history.back()}
-                  >
-                    <OrderInfo />
-                  </Modal>
-                }
-              />
-              <Route path='orders' element={<ProfileOrders />} />
-            </Route>
+            <Route path='/profile' element={<Profile />} />
+            <Route path='orders' element={<ProfileOrders />} />
           </Route>
           <Route path='*' element={<NotFound404 />} />
         </Routes>
+        {background && (
+          <Routes>
+            <Route
+              path='/feed/:number'
+              element={
+                <Modal
+                  title={`Информация о заказе`}
+                  onClose={() => navigate(-1)}
+                >
+                  <OrderInfo />
+                </Modal>
+              }
+            />
+            <Route
+              path='/ingredients/:id'
+              element={
+                <Modal
+                  title={`Детали ингредиента`}
+                  onClose={() => navigate(-1)}
+                >
+                  <IngredientDetails />
+                </Modal>
+              }
+            />
+            <Route
+              path='/profile/orders/:number'
+              element={
+                <ProtectedRoute>
+                  <Modal
+                    title={`#${location.pathname.split('/')[3]}`}
+                    onClose={() => navigate(-1)}
+                  >
+                    <OrderInfo />
+                  </Modal>
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        )}
       </div>
     </>
   );
